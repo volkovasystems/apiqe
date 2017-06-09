693fc8aa24296cca6af0e7a96657b43e8d9e1032
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-09 20:52:49 +0800

    Updated test.

--- apiqe-test.js
+++ apiqe-test.js
@@ -6,4 +6,7 @@ assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ), [ 1, 2, 3, 4, 5, 6
 
 assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ], "should be equal to [ 'hello', 'world' ]" );
 
+let test1 = apiqe( [ Array, Object ], [ Array, Object, Object, Date, RegExp ] );
+assert.deepEqual( test1, [ Array, Object, Date, RegExp ], "should be deeply equal" );
+
 console.log( "ok" );
