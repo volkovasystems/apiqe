91cbf180dc9796f46c8e98c72ad297d0778480f4
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 17:50:10 +0800

    Updated server/client test.

--- apiqe-test.js
+++ apiqe-test.js
@@ -2,11 +2,10 @@
 const assert = require( "assert" );
 const apiqe = require( "./apiqe.js" );
 
-assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ), [ 1, 2, 3, 4, 5, 6 ], "should be equal to [ 1, 2, 3, 4, 5, 6 ]" );
+assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ),
+	[ 1, 2, 3, 4, 5, 6 ], "should be equal to [ 1, 2, 3, 4, 5, 6 ]" );
 
-assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ], "should be equal to [ 'hello', 'world' ]" );
-
-let test1 = apiqe( [ Array, Object ], [ Array, Object, Object, Date, RegExp ] );
-assert.deepEqual( test1, [ Array, Object, Date, RegExp ], "should be deeply equal" );
+assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ],
+	"should be equal to [ 'hello', 'world' ]" );
 
 console.log( "ok" );
