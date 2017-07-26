039c4af7bb1ed155e02a041fd6385526f5f948b2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 11:02:20 +0800

    Updated test.

--- apiqe-test.js
+++ apiqe-test.js
@@ -3,9 +3,9 @@ const assert = require( "assert" );
 const apiqe = require( "./apiqe.js" );
 
 assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ),
-	[ 1, 2, 3, 4, 5, 6 ], "should be equal to [ 1, 2, 3, 4, 5, 6 ]" );
+	[ 1, 2, 3, 4, 5, 6 ], "should return [ 1, 2, 3, 4, 5, 6 ]" );
 
 assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ],
-	"should be equal to [ 'hello', 'world' ]" );
+	"should return [ 'hello', 'world' ]" );
 
 console.log( "ok" );
