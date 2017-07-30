ebc7bc9e6417f8404abf8e0faab753aff33248b1
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-30 14:08:06 +0800

    Save changes.

---
+++ test.module.js
@@ -0,0 +1,144 @@
+"use strict";
+
+/*;
+	@test-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-test-license
+
+	@test-configuration:
+		{
+			"package": "apiqe",
+			"path": "apiqe/test.module.js",
+			"file": "test.module.js",
+			"module": "test",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/apiqe.git"
+		}
+	@end-test-configuration
+
+	@test-documentation:
+
+	@end-test-documentation
+
+	@include:
+		{
+			"assert": "should",
+			"apiqe": "apiqe"
+		}
+	@end-include
+*/
+
+const assert = require( "should" );
+
+//: @server:
+const apiqe = require( "./apiqe.js" );
+//: @end-server
+
+//: @client:
+const apiqe = require( "./apiqe.support.js" );
+//: @end-client
+
+//: @bridge:
+const path = require( "path" );
+//: @end-bridge
+
+
+//: @server:
+
+describe( "apiqe", ( ) => {
+
+	describe( "`apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] )`", ( ) => {
+		it( "should be equal to [ 1, 2, 3, 4, 5, 6 ]", ( ) => {
+
+			assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ),
+			[ 1, 2, 3, 4, 5, 6 ] );
+
+		} );
+	} );
+
+	describe( "`apiqe( 'hello', 'world' )`", ( ) => {
+		it( "should be equal to [ 'hello', 'world' ]", ( ) => {
+			assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ] );
+		} );
+	} );
+
+} );
+
+//: @end-server
+
+//: @client:
+
+describe( "apiqe", ( ) => {
+
+	describe( "`apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] )`", ( ) => {
+		it( "should be equal to [ 1, 2, 3, 4, 5, 6 ]", ( ) => {
+
+			assert.deepEqual( apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ),
+			[ 1, 2, 3, 4, 5, 6 ] );
+
+		} );
+	} );
+
+	describe( "`apiqe( 'hello', 'world' )`", ( ) => {
+		it( "should be equal to [ 'hello', 'world' ]", ( ) => {
+			assert.deepEqual( apiqe( "hello", "world" ), [ "hello", "world" ] );
+		} );
+	} );
+
+} );
+
+//: @end-client
+
+//: @bridge:
+
+describe( "apiqe", ( ) => {
+
+	const testBridge = path.resolve(__dirname, "bridge.html");
+	const bridgeURL = "file://" + testBridge;
+
+	describe( "`apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] )`", ( ) => {
+		it( "should be equal to [ 1, 2, 3, 4, 5, 6 ]", ( ) => {
+
+			let result = browser.url( bridgeURL ).execute( ( ) => apiqe( [ 1, 2 ,3 ], [ 4, 5, 6, 1, 2, 3 ] ) );
+			assert.deepEqual( result.value, [ 1, 2, 3, 4, 5, 6 ] );
+
+		} );
+	} );
+
+	describe( "`apiqe( 'hello', 'world' )`", ( ) => {
+		it( "should be equal to [ 'hello', 'world' ]", ( ) => {
+			let result = browser.url( bridgeURL ).execute( ( ) => apiqe( "hello", "world" ) );
+			assert.deepEqual( result.value, [ "hello", "world" ] );
+		} );
+	} );
+
+} );
+
+//: @end-bridge
